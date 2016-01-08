d6465f5d14c2a02d13019b637eae0ae585d53c75
Alexandre Crettet <alexandre.crettet@epitech.eu>
2016-01-08 22:07:28 +0100

    Add fake data for the Ranking view

--- app/src/main/java/com/peppersalt/peppersalt/ranking/RankingFragment.java
+++ app/src/main/java/com/peppersalt/peppersalt/ranking/RankingFragment.java
@@ -7,13 +7,44 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.peppersalt.peppersalt.R;
+import com.peppersalt.peppersalt.api.model.Person;
 import com.peppersalt.peppersalt.base.PepperSaltFragment;
+import com.peppersalt.peppersalt.customviews.podiumView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RankingFragment extends PepperSaltFragment {
+
+  @Bind(R.id.podiumView) podiumView podiumV;
 
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_ranking, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ButterKnife.bind(this, view);
+    setFakeData();
+  }
+
+  private void setFakeData() {
+    List<Person> people = new ArrayList<>();
+    Person person = new Person();
+
+    person.setFirstName("Noel");
+    person.setLastName("Flantier");
+    person.setPoints(10);
+    person.setUsername("flanti_n");
+    people.add(person);
+    people.add(person);
+    people.add(person);
+    podiumV.setData(people);
   }
 }
