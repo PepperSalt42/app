4f311d0e45bc933fcbfa6087ee7be81b211a90ba
Alexandre Crettet <alexandre.crettet@epitech.eu>
2016-01-09 20:23:53 +0100

    Retrieve the last announce from the server

--- app/src/main/java/com/peppersalt/peppersalt/announce/AnnounceFragment.java
+++ app/src/main/java/com/peppersalt/peppersalt/announce/AnnounceFragment.java
@@ -7,23 +7,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.peppersalt.peppersalt.R;
+import com.peppersalt.peppersalt.api.RestClient;
+import com.peppersalt.peppersalt.api.RestService;
+import com.peppersalt.peppersalt.api.model.Announce;
 import com.peppersalt.peppersalt.base.PepperSaltFragment;
 import com.peppersalt.peppersalt.utils.RoundedTransformation;
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class AnnounceFragment extends PepperSaltFragment {
 
-  @Bind(R.id.announceImage1) ImageView announceImageView1;
+  @Bind(R.id.announceImage) ImageView announceImageView;
+  @Bind(R.id.progress_bar) ProgressBar progressBar;
+  @Bind(R.id.empty_view) TextView emptyView;
+  @Bind(R.id.error_view) TextView errorView;
 
   private String url1;
   private boolean url1Changed = true;
+  private Context context;
 
-  private static int REFRESH_TIME = 60000;
+  private static int REFRESH_TIME = 10000;
 
   @Nullable
   @Override
@@ -34,37 +46,66 @@ public class AnnounceFragment extends PepperSaltFragment {
 
   @Override
   public void onViewCreated(final View view, Bundle savedInstanceState) {
-    final Context context = view.getContext();
     super.onViewCreated(view, savedInstanceState);
     ButterKnife.bind(this, view);
 
     final Runnable runnable = new Runnable() {
       @Override
       public void run() {
-        getAnnounceUrl();
-
-        if (url1Changed) {
-          int radius = (int) context.getResources().getDimension(R.dimen.announce_corner_radius);
-
-          Picasso.with(context)
-              .load(url1)
-              .transform(new RoundedTransformation(radius, 0))
-              .into(announceImageView1);
-          url1Changed = false;
-        }
+        getAnnounceUrl(view);
         view.postDelayed(this, REFRESH_TIME);
       }
     };
-    view.postDelayed(runnable, 2000);
+    view.post(runnable);
   }
 
-  private void getAnnounceUrl() {
-    String url;
+  private void getAnnounceUrl(final View view) {
+    RestService service = RestClient.getInstance().getRestService();
 
-    url = "https://ludovicpost.me/announce.png ";
-    if (!url.equals(url1)) {
-      url1 = url;
-      url1Changed = true;
-    }
+    service.getLastImage(new Callback<Announce>() {
+      @Override
+      public void success(Announce announce, Response response) {
+        String url = announce.getImageUrl();
+        Context context = view.getContext();
+
+        if (!url.equals(url1)) {
+          int radius = (int) context.getResources().getDimension(R.dimen.announce_corner_radius);
+          url1 = url;
+
+          Picasso.with(context)
+              .load(url1)
+              .error(R.drawable.background_fragment_default)
+              .transform(new RoundedTransformation(radius, 0))
+              .into(announceImageView);
+        }
+        showContent();
+      }
+
+      @Override
+      public void failure(RetrofitError error) {
+        showError();
+      }
+    });
+  }
+
+  private void showContent() {
+    announceImageView.setVisibility(View.VISIBLE);
+    progressBar.setVisibility(View.GONE);
+    errorView.setVisibility(View.GONE);
+    emptyView.setVisibility(View.GONE);
+  }
+
+  private void showEmpty() {
+    announceImageView.setVisibility(View.GONE);
+    progressBar.setVisibility(View.GONE);
+    errorView.setVisibility(View.GONE);
+    emptyView.setVisibility(View.VISIBLE);
+  }
+
+  private void showError() {
+    announceImageView.setVisibility(View.GONE);
+    progressBar.setVisibility(View.GONE);
+    emptyView.setVisibility(View.GONE);
+    errorView.setVisibility(View.VISIBLE);
   }
 }
