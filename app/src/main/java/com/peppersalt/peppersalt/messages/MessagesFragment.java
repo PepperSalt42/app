a38b7481758ee04cf972e8b86ae2d45fd2d76fe0
Alexandre Crettet <alexandre.crettet@epitech.eu>
2016-01-10 16:28:32 +0100

    Remove fake data from messages (dead code)

--- app/src/main/java/com/peppersalt/peppersalt/messages/MessagesFragment.java
+++ app/src/main/java/com/peppersalt/peppersalt/messages/MessagesFragment.java
@@ -134,23 +134,4 @@ public class MessagesFragment extends PepperSaltLceFragment {
       }
     }
   }
-
-  private void addFakeData() {
-    Person author = new Person();
-    Message message = new Message();
-
-    author.setFirstName("Lucien");
-    author.setLastName("Bramart");
-    author.setUsername("bramar_l");
-    author.setImageUrl("https://avatars.slack-edge.com/2016-01-06/17850350192_00b38f75688300858435_192.jpg");
-    author.setPoints(10);
-    message.setAuthor(author);
-    message.setTime("2016-15-08 18:54:42");
-    message.setMessage("Bonjour, ceci est un test de la team PepperSalt Android. Ceci est un message allongé afin de prendre plus d'espace");
-
-    for (int i = 0; i < 7; ++i) {
-      data.add(message);
-    }
-    adapter.setData(data);
-  }
 }
